9e0ee57275ed8194fb894f27579f112219b529af
shohrux508 <yigitalievshohrux@gmail.com>
2025-04-23 20:48:05 +0500

    ....

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 import asyncio
 import websockets
-from manager import Ports
+import json
 
 async def websocket_client():
-    uri = "ws://127.0.0.1:8000/devices/1"
-    arduino = Ports(port_name='COM14', baud_rate=9600)
+    uri = "wss://shiot-production.up.railway.app/devices/3"
     async with websockets.connect(uri) as websocket:
         while True:
-            cmd = await arduino.read_commands()
-            if cmd:
-                await websocket.send(cmd)
-                print(f"→ Sent to WS: {cmd!r}")
+            await websocket.send(json.dumps({"token":"2645"}))
             try:
                 msg = await websocket.recv()
             except websockets.ConnectionClosed:
                 print("WebSocket закрыт, выходим из цикла")
                 break
-            print(f"← Received from WS: {msg!r}")
-            await arduino.send_command(msg)
+            if msg == 'ping':
+                await websocket.send('pong')
             await asyncio.sleep(0.01)
 
 if __name__ == "__main__":
