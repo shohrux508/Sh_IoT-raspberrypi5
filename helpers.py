71632bef6263f8026a3aa2938952ffc9d7d52460
shohrux508 <yigitalievshohrux@gmail.com>
2025-05-24 03:09:48 +0500

    Refactor the code by replacing the Arduino management class with event-driven websocket handling and updating dependencies in requirements.txt.

--- helpers.py
+++ helpers.py
@@ -2,3 +2,6 @@
 
 def filter_data(data, function):
     return data.replace(function, '')
+
+
+print(filter_data('start+1234,5678', 'start+').split(','))
--- main.py
+++ main.py
@@ -1,76 +1,46 @@
-import asyncio
-import time
+import json
 
 import websockets
+import asyncio
+from pyee.asyncio import AsyncIOEventEmitter
 
-from helpers import filter_data
-
-
-class manageArduino:
-    def __init__(self, **pins):
-        self.pins = pins
-        self.pins_status = {key: 0 for key, value in pins.items()}
-        self.pins_in_num = {int(key.replace('pin', '')): key for key, value in pins.items()}
-
-    def get_pin(self, pin: int):
-        pin_name = self.pins_in_num.get(pin)
-        if pin_name is None:
-            print(f"[Предупреждение] Пин {pin} не найден.")
-            return None
-        return pin_name
-
-    def get_status(self, pin: int):
-        key = self.get_pin(pin)
-        if key is None:
-            return None
-        return self.pins_status.get(key)
-
-    def toggle(self, pin: int = None):
-        if pin is None:
-            for key, value in self.pins_status.items():
-                self.pins_status[key] = int(not value)
-        else:
-            key = self.get_pin(pin)
-            if key is not None:
-                self.pins_status[key] = int(not self.pins_status[key])
-        return self.get_status(pin) if pin else None
-
-    def set_pin(self, pin: int, status: bool):
-        key = self.get_pin(pin)
-        if key is not None:
-            self.pins_status[key] = int(status)
-        return self.get_status(pin)
-
-
-
-arduino = manageArduino(pin2='led1', pin3='led2', pin4='led3')
-arduino.set_pin(pin=2, status=1)
-print(arduino.pins_status)
+event_bus = AsyncIOEventEmitter()
+local_url = 'ws://127.0.0.1:8000/devices/ws/3/connect'
+pub_url = "wss://shiot-production.up.railway.app/devices/ws/3/connect"
 
 
 async def websocket_client():
-    uri = "wss://shiot-production.up.railway.app/devices/register/3"
+    uri = pub_url
     async with websockets.connect(uri) as websocket:
         print("Connected to server.")
+        data = {'auth_token': 'abc12345'}
+
+        await websocket.send(json.dumps(data))
         while True:
             try:
                 msg = await websocket.recv()
                 if msg == 'ping':
                     await websocket.send('pong')
                 else:
-                    print(f"Получено: {msg}")
-                    if msg.startswith('set_pin:'):
-                        # set_pin:5-1
-                        pin, status = (filter_data('set_pin:')).split('-')
-                        arduino.set_pin(pin, status)
-                    if msg.startswith('toggle_pin:'):
-                        pin = int(filter_data(msg, 'toggle_pin:'))
-                        arduino.toggle(pin=pin)
+                    try:
+                        data = json.loads(msg)
+                    except json.JSONDecodeError:
+                        print(msg)
+                        continue
+                    event_bus.emit('message_from_server', data, websocket)
 
             except websockets.ConnectionClosed:
                 print("Connection closed by server.")
                 break
             await asyncio.sleep(0.01)
+
+
+@event_bus.on('message_from_server')
+async def handle_message(msg, ws):
+    request_id = msg.get('request_id', None)
+    action = msg.get('action', None)
+    data = {'action': action, 'status': 'done', 'request_id': request_id}
+    await ws.send(json.dumps(data))
 
 
 if __name__ == "__main__":
